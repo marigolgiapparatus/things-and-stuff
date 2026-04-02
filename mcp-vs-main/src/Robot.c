6e4950e0fe5942299fa4b1912022ce75af00f4c8
marigolgiapparatus <kri.spytroll@gmail.com>
2026-04-02 11:01:54 +0800

    Update Robot.c

--- mcp-vs-main/src/Robot.c
+++ mcp-vs-main/src/Robot.c
@@ -7,70 +7,55 @@
 #include "../lib/adc/adc.h"
 
 // static function prototypes, functions only called in this file
-volatile uint32_t milliseconds = 0;
-char serialString[100] = {};
 
 int main(void)
 {
-  // set up whatever
+  //init adc
+  adc_init();
+  _delay_ms(20);
+
+  // joystick & switch
+
+
+  // init pwm
+
+  // 0-1-2-3 -> 10-11-12-13
+
+  TCCR1A = 0; TCCR1B = 0;
+
+  TCCR1A |= (1<<COM1B1) | (1<<COM1A1);
+  TCCR1B |= (1<<WGM13) | (1<<CS11);
+
+  ICR1 = 20000; // sets frequency (needs calc) - sets top
+
+  DDRB |= (1<<PB5)|(1<<PB6); 
+
+  uint16_t compValue = 1600;
+  uint16_t compValue2 = 1600;
+  OCR1A = compValue;
+  OCR1B = compValue2;
+
+  // joystick val
+  uint16_t xVal = 0;
+  uint16_t yVal = 0;
+
+  // servo output setup
   serial0_init();
 
-  // internal clock
-  TCCR5A = 0;
-  TCCR5B = (1<<WGM52);
-  TCNT5 = 0; // reset timer
-  OCR5A = 15999; // 1ms at 16MHz
-  TIMSK5 |= (1<<OCIE5A); // output compare
-  TCCR5B |= (1<<CS50); // prescaler 1
-
-
-  // button set up
-  cli(); // closes the interrupt
-  EICRA |= (1 << ISC31); //| (1 << ISC30);
-  EIMSK |= (1 << INT3);
-
-  EICRA |= (1<<ISC21); //| (1<<ISC20);
-  EIMSK |= (1<<INT2);
-
-  sei(); // turns on the interrupt
-
-  // set up timer
 
   while (1) {
-    // set up main code
-    _delay_ms(20);
-    uint32_t mins = milliseconds / 3600000;
-    uint32_t secs = (milliseconds % 60000) / 1000;
-    uint32_t ms = milliseconds % 1000;
-    sprintf(serialString, "%lumin %lusec %lums total: %u\n", mins, secs, ms, milliseconds); 
-    // sprintf(serialString, "%lu sec %u ms\n", secs, ms);
+    xVal = adc_read(2);
+    yVal = adc_read(1); // read joystick y val
 
-    serial0_print_string(serialString);
+    // map joystick val to pwm comp value
+    compValue = xVal * 1.758 + 620; // map 620-2420
 
-    // set up button interrupt
+    compValue2 = yVal * 1.758 + 620; // map 620-2420
+
+    OCR1A = compValue; // set pwm duty cycle
+
+    OCR1B = compValue2; // set pwm duty cycle for second servo
   }
 
   return (1);
 }
-
-// button 1 (pause) interrupt code
-ISR(INT2_vect) {
-  // button pauses until its pressed again
-  if (TCCR5B & (1<<CS50)) {
-    TCCR5B &= ~(1<<CS50); // stop timer
-  } else {
-    TCCR5B |= (1<<CS50); // start timer
-  } 
-
-  return;
-}
-// button 2 (reset) interrupt code
-ISR(INT3_vect) {
-  milliseconds = 0;
-  return;
-}
-
-ISR(TIMER5_COMPA_vect) {
-  milliseconds++;
-}
-
